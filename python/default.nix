7a7f21d4f21906da235ef8e7a0be0fc9e40a7542
Kai Wohlfahrt <kai@prodo.ai>
2018-10-19 16:44:05 +0100

    Add semi-composable python overlay
    Need to do `lib.composeExtensions (...) super.pythonOverrides` to
    combine. See nixpkgs#44426.

--- python/default.nix
+++ python/default.nix
@@ -1,9 +1,16 @@
-self: super: {
+self: super:
+
+let
+  empty = self: super: {};
+in {
+  # nixpkgs#44426
+  pythonOverrides = super.lib.composeExtensions (self: super: {
+    dotmap = super.callPackage ./dotmap.nix {};
+    tensorboardX = super.callPackage ./tensorboardX.nix {};
+    hyperopt = super.callPackage ./hyperopt.nix {};
+  }) (super.pythonOverrides or empty);
+
   python3 = super.python3.override {
-    packageOverrides = self: super: {
-      dotmap = super.callPackage ./dotmap.nix {};
-      tensorboardX = super.callPackage ./tensorboardX.nix {};
-      hyperopt = super.callPackage ./hyperopt.nix {};
-    };
+    packageOverrides = self.pythonOverrides;
   };
 }
