f1b93921007cb356c20153355e5a3367fd52904b
Kai Wohlfahrt <kai@prodo.ai>
2018-10-31 13:56:36 +0000

    Use builtins.fetchGit
    This allows importing from derivation

--- apps/gomod2nix.nix
+++ apps/gomod2nix.nix
@@ -1,10 +1,9 @@
-{ callPackage, fetchgit }:
+{ callPackage }:
 
 # I expect this to be a common pattern, might need factoring out
 let
-  src = fetchgit {
+  src = builtins.fetchGit {
     url = "https://github.com/kwohlfahrt/gomod2nix.git";
     rev = "7a7b43a48b637a9ed94ee505c8bc2fb1a331a842";
-    sha256 = "1x4nq7idk9qhifd1f6c8xjw4g5li4ldfg2i9ddmm7gk3l8lhxarl";
   };
-in callPackage (import "${src.out}/gomod2nix.nix") {}
+in callPackage (import "${src}/gomod2nix.nix") {}
