2f9805fe3c5f311c18b447fab5bc04a09a11e4d2
Kai Wohlfahrt <kai@prodo.ai>
2019-01-25 11:47:52 +0000

    Fix yacs tests

--- python/yacs.nix
+++ python/yacs.nix
@@ -1,19 +1,19 @@
-{ buildPythonPackage, fetchPypi, pyyaml }:
+{ buildPythonPackage, fetchFromGitHub, pyyaml }:
 
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "yacs";
-  version = "0.1.4";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0mqg5lz4jsg5b94nnxq89976d4pq0b589l6pfgxd1l14jbc8pr1a";
+  version = "0.1.5";
+  src = fetchFromGitHub {
+    owner = "rbgirshick";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0h6cgya3i10vqnzzd8xvzwz9x86zgfpgdw8isq45psbp7l516yi9";
   };
 
   propagatedBuildInputs = [ pyyaml ];
 
-  # Issue 7
-  doCheck = false;
-  checkPhase = ''
+  installCheckPhase = ''
     python yacs/tests.py
   '';
 }
