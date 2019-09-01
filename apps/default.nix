1e4f9ef50ded9156358939860394185465496bda
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-09-01 18:28:53 +0100

    dtc from git master

--- apps/default.nix
+++ apps/default.nix
@@ -4,4 +4,11 @@ self: super: {
   vipaccess = super.callPackage ./vipaccess.nix {};
   dyndns = super.callPackage ./dyndns.nix {};
   plz-cli = super.callPackage ./plz-cli.nix {};
+  dtc_git = super.dtc.overrideAttrs (o: rec {
+    version = "git";
+    src = o.src.overrideAttrs (o: {
+      rev = "b8d6eca78210952c6d93235c38ebd5836d6409c4";
+      sha256 = "0a8rxw5xkm9jb3lh53sqk7d0yxir4mwf3m8rs9bl6ibdw2z5ivmy";
+    });
+  });
 }
