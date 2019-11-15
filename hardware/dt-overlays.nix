45ad4685add1949119bf761fb8e4603747b65a7f
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-11-15 17:39:55 +0000

    Bump dt-overlays

--- hardware/dt-overlays.nix
+++ hardware/dt-overlays.nix
@@ -2,6 +2,6 @@
 let
   src = builtins.fetchGit {
     url = "https://github.com/kwohlfahrt/dt-overlays.git";
-    rev = "e6a5eea656a26134e76eee2f2e53f40c2911aa31";
+    rev = "9e3749d1bd3dbda3a54c1206954018819b0a2312";
   };
 in import "${src}/dt-overlays.nix"
