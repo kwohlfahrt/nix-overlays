b70be8c5694ea4ec256d452bf54c22a417caa64f
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-12-05 13:34:47 +0000

    Remove DTC git

--- apps/default.nix
+++ apps/default.nix
@@ -5,11 +5,4 @@ self: super: {
   dyndns = super.callPackage ./dyndns.nix {};
   pimostat = super.callPackage ./pimostat.nix {};
   plz-cli = super.callPackage ./plz-cli.nix {};
-  dtc_git = super.dtc.overrideAttrs (o: rec {
-    version = "git";
-    src = o.src.overrideAttrs (o: {
-      rev = "b8d6eca78210952c6d93235c38ebd5836d6409c4";
-      sha256 = "0a8rxw5xkm9jb3lh53sqk7d0yxir4mwf3m8rs9bl6ibdw2z5ivmy";
-    });
-  });
 }
