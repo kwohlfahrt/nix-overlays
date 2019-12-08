1e4df0c2439a160a3132ae691cd0aad193974b58
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-12-08 22:35:21 +0000

    quartus: Add working ModelSim
    This requires buildFHSUserEnv, because modelsim_ase/linux/vlm seems to
    checksum itself.

--- apps/default.nix
+++ apps/default.nix
@@ -1,6 +1,6 @@
 self: super: {
   gomod2nix = super.callPackage ./gomod2nix.nix {};
-  quartus-prime-lite = super.callPackage ./quartus-prime-lite.nix {};
+  quartus-prime-lite = super.callPackage ./quartus-prime-lite {};
   oath = super.callPackage ./oath.nix {};
   vipaccess = super.callPackage ./vipaccess.nix {};
   dyndns = super.callPackage ./dyndns.nix {};
